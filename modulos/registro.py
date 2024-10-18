b93b35ff8db148d0702a2ad894176ca2c77b6cc5
Lautarop <pelozo.lautaro.e@gmail.com>
2024-10-18 17:02:45 -0300

    cuarto commit

--- modulos/registro.py
+++ modulos/registro.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
 def iniciar_registro():
-    with open('ingDatos/log/registro_actividades.txt', 'a') as archivo_log:
+    with open('log/registro_actividades.txt', 'a') as archivo_log:
         archivo_log.write(f"Registro de actividades - {datetime.now()}\n")
 
 def registrar(mensaje):
-    with open('ingDatos/log/registro_actividades.txt', 'a') as archivo_log:
+    with open('log/registro_actividades.txt', 'a') as archivo_log:
         archivo_log.write(f"{datetime.now()} - {mensaje}\n")
