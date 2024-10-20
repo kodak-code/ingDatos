6a4a502eed1bc387999b9f47b2c25f83e5258689
Matias Mamani <matiasmamani163@gmail.com>
2024-10-19 23:31:12 -0300

    excepciones y mas temas de la materia

--- moduloDB/acceso_datos.py
+++ moduloDB/acceso_datos.py
@@ -4,6 +4,8 @@ import os
 from moduloDB.registro import registrar_actividad
 
 def insertar_datos_csv():
+    contrataciones_procesadas = set()  # evitar contrataciones duplicadas
+
     try:
         conn = sqlite3.connect('base1.db')
         c = conn.cursor()
@@ -12,44 +14,53 @@ def insertar_datos_csv():
         try:
             archivo = open(carpeta + 'compras-y-contrataciones-sss-20190826.csv', 'r', encoding='utf-8')
         except FileNotFoundError:
-            print("El fichero al que se intenta acceder no existe.")
-            registrar_actividad("El fichero al que se intenta acceder no existe.")
+            mensaje = "El fichero al que se intenta acceder no existe."
+            print(mensaje)
+            registrar_actividad(mensaje)
             return
 
         lista = csv.reader(archivo, delimiter=',')
         next(lista)
 
         for linea in lista:
-            try:
-                mensaje = f"Ingresando datos: {linea}"
-                print(mensaje)
-                registrar_actividad(mensaje)
+            if linea[0] not in contrataciones_procesadas:  # verifica si ya se proceso la contratacion
+                contrataciones_procesadas.add(linea[0])  # añade  al set
+                try:
+                    mensaje = f"Ingresando datos: {linea}"
+                    print(mensaje)
+                    registrar_actividad(mensaje)
 
-                strSQL = "INSERT INTO Estado VALUES(?, ?)"
-                conn.execute(strSQL, (linea[0], linea[3]))
+                    strSQL = "INSERT INTO Estado VALUES(?, ?)"
+                    conn.execute(strSQL, (linea[0], linea[3]))
 
-                strSQL = "INSERT INTO Contratacion VALUES(?, ?)"
-                conn.execute(strSQL, (linea[0], linea[2]))
+                    strSQL = "INSERT INTO Contratacion VALUES(?, ?)"
+                    conn.execute(strSQL, (linea[0], linea[2]))
 
-                strSQL = "INSERT INTO Fecha VALUES(?, ?)"
-                conn.execute(strSQL, (linea[0], linea[4]))
+                    strSQL = "INSERT INTO Fecha VALUES(?, ?)"
+                    conn.execute(strSQL, (linea[0], linea[4]))
 
-                strSQL = "INSERT OR IGNORE INTO Practica VALUES(?, ?, ?, ?, ?, ?)"
-                conn.execute(strSQL, (linea[0], linea[1], linea[2], linea[3], linea[4], linea[5]))
-            except IndexError as e:
-                print(f"Error en el índice de la lista: {e}")
-                registrar_actividad(f"Error en el índice de la lista: {e}")
-            except sqlite3.IntegrityError as e:
-                print(f"Error de integridad en la base de datos: {e}")
-                registrar_actividad(f"Error de integridad en la base de datos: {e}")
+                    strSQL = "INSERT OR IGNORE INTO Practica VALUES(?, ?, ?, ?, ?, ?)"
+                    conn.execute(strSQL, (linea[0], linea[1], linea[2], linea[3], linea[4], linea[5]))
+
+                except IndexError as e:
+                    mensaje = f"Error en el índice de la lista: {e}"
+                    print(mensaje)
+                    registrar_actividad(mensaje)
+                except sqlite3.IntegrityError as e:
+                    mensaje = f"Error de integridad en la base de datos: {e}"
+                    print(mensaje)
+                    registrar_actividad(mensaje)
 
         conn.commit()
+
     except sqlite3.DatabaseError as e:
-        print(f"Error en la base de datos: {e}")
-        registrar_actividad(f"Error en la base de datos: {e}")
+        mensaje = f"Error en la base de datos: {e}"
+        print(mensaje)
+        registrar_actividad(mensaje)
     except Exception as e:
-        print(f"Error inesperado: {e}")
-        registrar_actividad(f"Error inesperado: {e}")
+        mensaje = f"Error inesperado: {e}"
+        print(mensaje)
+        registrar_actividad(mensaje)
     finally:
         archivo.close()
         conn.close()
